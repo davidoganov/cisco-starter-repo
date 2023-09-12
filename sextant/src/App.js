36850a9c0a9ec4c6c512757065af5e9423b710d9
davidoganov <112603130+davidoganov@users.noreply.github.com>
2023-09-11 22:19:41 -0700

    Update App.js

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,48 +1,51 @@
+import React, { Component } from 'react';
 import Icon from './Icon.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sextant</h1>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Sextant</h1>
 
-        <br></br>
+          <br></br>
 
-        <img src={Icon} className="App-logo" alt="logo" />  {/*Icon image credit: https://freeicons.io/profile/6156 */}
+          <img src={Icon} className="App-logo" alt="logo" />  {/*Icon image credit: https://freeicons.io/profile/6156 */}
 
-        <br></br>
-        <br></br>
-        
-        <button style={{
-          fontSize: '50px', fontWeight: 'bold', fontFamily: '-moz-initial',
-          backgroundColor: 'rgba(45, 202, 195, 0.8)', marginBottom: '5px'
-        }}>
-          IP Information</button>
-        <button style={{
-          fontSize: '50px', fontWeight: 'bold', fontFamily: '-moz-initial',
-          backgroundColor: 'rgba(45, 202, 195, 0.8)', marginBottom: '5px'
-        }}>
-          Latency Metrics</button>
+          <br></br>
+          <br></br>
 
-        <br></br>
-        
-        <div>
           <button style={{
-            fontSize: '36px', fontWeight: 'bold', fontFamily: '-moz-initial',
-            backgroundColor: 'rgba(0, 253, 181, 0.45)', marginRight: '5px'
+            fontSize: '50px', fontWeight: 'bold', fontFamily: '-moz-initial',
+            backgroundColor: 'rgba(45, 202, 195, 0.8)', marginBottom: '5px'
           }}>
-            Deploy</button>
+            IP Information</button>
           <button style={{
-            fontSize: '36px', fontWeight: 'bold', fontFamily: '-moz-initial',
-            backgroundColor: 'rgba(0, 253, 181, 0.45)', marginRight: '5px'
+            fontSize: '50px', fontWeight: 'bold', fontFamily: '-moz-initial',
+            backgroundColor: 'rgba(45, 202, 195, 0.8)', marginBottom: '5px'
           }}>
-            Troubleshoot</button>
-        </div>
+            Latency Metrics</button>
 
-      </header>
-    </div>
-  );
+          <br></br>
+
+          <div>
+            <button style={{
+              fontSize: '36px', fontWeight: 'bold', fontFamily: '-moz-initial',
+              backgroundColor: 'rgba(0, 253, 181, 0.45)', marginRight: '5px'
+            }}>
+              Deploy</button>
+            <button style={{
+              fontSize: '36px', fontWeight: 'bold', fontFamily: '-moz-initial',
+              backgroundColor: 'rgba(0, 253, 181, 0.45)', marginRight: '5px'
+            }}>
+              Troubleshoot</button>
+          </div>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
